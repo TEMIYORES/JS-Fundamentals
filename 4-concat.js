3ab5797055dffd4309664568c207f1d52cca22b8
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-05-21 12:10:42 +0100

    eight commit

---
+++ 4-concat.js
@@ -0,0 +1,10 @@
+const { argv } = require("node:process");
+const args = argv.slice(2);
+
+if (args[0] && !args[1]) {
+  console.log(`${args[0]} is `);
+} else if (args[0] && args[1]) {
+  console.log(`${args[0]} is ${args[1]}`);
+} else {
+  console.log(`undefined is undefined`);
+}
