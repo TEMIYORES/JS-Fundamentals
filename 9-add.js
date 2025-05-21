5e9aba9eebedbcdd176819e8a9d566e8b610f2a9
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-05-21 13:42:01 +0100

    9-add second commit

--- 9-add.js
+++ 9-add.js
@@ -2,7 +2,6 @@ const { argv } = require("node:process");
 const args = argv.slice(2);
 
 if (args[0] && args[1]) {
-  console.log(args);
   console.log(add(args[0], args[1]));
 } else {
   console.log(NaN);
