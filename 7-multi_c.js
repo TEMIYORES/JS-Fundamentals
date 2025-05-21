fee96036aa4641cf44f759fcdbbc289df560eed4
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-05-21 12:48:45 +0100

    7-multi_c first push

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const { argv } = require("node:process");
+const args = argv.slice(2);
+
+if (args[0]) {
+  let num = Number(args[0]);
+  let i = 0;
+  while (i < num) {
+    console.log("C is fun");
+    i++;
+  }
+}
