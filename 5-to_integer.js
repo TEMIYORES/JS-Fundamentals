3b774145ee0b1b3dab33d14f130b94b3e6a36da1
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-05-21 12:19:47 +0100

    5-to_integer second push

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -4,12 +4,8 @@ const args = argv.slice(2);
 if (args[0]) {
   let num = Number(args[0]);
   if (!(num == "NaN")) {
-    console.log(`${Math.floor(num)}`);
+    console.log(`My number: ${Math.floor(num)}`);
   } else {
     console.log("Not a number");
   }
-} else if (args[0] && args[1]) {
-  console.log(`${args[0]} is ${args[1]}`);
-} else {
-  console.log(`undefined is undefined`);
 }
