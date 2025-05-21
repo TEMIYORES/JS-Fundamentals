b776a67a394bb4a73599d2eff9a1c0d52439b456
Qayyum Ibrahim <temiyores1234@gmail.com>
2025-05-21 12:11:41 +0100

    4-concat second push

--- 4-concat.js
+++ 4-concat.js
@@ -2,7 +2,7 @@ const { argv } = require("node:process");
 const args = argv.slice(2);
 
 if (args[0] && !args[1]) {
-  console.log(`${args[0]} is `);
+  console.log(`${args[0]} is undefined`);
 } else if (args[0] && args[1]) {
   console.log(`${args[0]} is ${args[1]}`);
 } else {
